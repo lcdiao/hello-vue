29eafbd974a4baadd515b0c47c975ffcd4bfa2b3
雕 <120879566@qq.com>
2019-09-09 22:09:57 +0800

    第一个路由

--- vue-cli/hello-vue-cli/src/App.vue
+++ vue-cli/hello-vue-cli/src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <router-link to="/">首页</router-link>
+    <router-link to="/content">内容页</router-link>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
---
+++ vue-cli/hello-vue-cli/src/components/Content.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div>
+      这是内容页,欢迎
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Content"
+    }
+</script>
+
+<style scoped>
+
+</style>
