0b30f4165446405d4f58d29870e8aaa98862698a
lcdiao <120879566@qq.com>
2019-09-11 18:01:20 +0800

    路由钩子与异步请求

--- hello-vue-element/src/main.js
+++ hello-vue-element/src/main.js
@@ -8,6 +8,10 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import axios from 'axios'
+/* 原型链 */
+Vue.prototype.axios = axios;
+
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
--- hello-vue-element/src/views/user/List.vue
+++ hello-vue-element/src/views/user/List.vue
@@ -7,8 +7,30 @@
 
 <script>
     export default {
-        props: ["id"],
-        name: "List"
+       props: ["id"],
+      name: "List",
+      beforeRouteEnter: (to, form, next) => {
+        console.log("页面进入前");
+        next(vm => {
+          vm.getData();
+        });
+      },
+      beforeRouteLeave: (to, form, next) => {
+        console.log("页面离开前");
+        next();
+      },
+      methods: {
+         getData: function () {
+           this.axios({
+             type: 'get',
+             url: 'http://localhost:8080/static/data.json'
+           }).then(response => {
+             console.log(response);
+           }).catch(error => {
+             console.log(error);
+           });
+         }
+      }
     }
 </script>
 
